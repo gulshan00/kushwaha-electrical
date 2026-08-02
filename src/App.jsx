bba1747542713505d37b32aa170512fb5cf71809
gulshan <gulshankiul1810@gmail.com>
2026-08-02 11:27:47 +0530

    home page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/Pages/Home";
+import Home from "./Components/pages/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Common/Footer";
 function App() {
